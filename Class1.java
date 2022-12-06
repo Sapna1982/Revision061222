0aec785accf59680a2226dbe812a67ec87205cc9
Sapna1982 <sapnasharma700@hotmail.com>
2022-12-06 20:14:16 +0000

    first commit 1.0

---
+++ Class1.java
@@ -0,0 +1,7 @@
+package Revision11;
+
+public class Class1 { public static void main (String[] args)
+{System.out.println("Software Testing");
+System.out.println("Manual Testing");
+System.out.println("Automation Testing");}
+}
